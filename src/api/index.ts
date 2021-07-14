b812cf28d19089afbd984b2b788330a8ac8b0f47
shashank7099 <shashanksaxena799@gmail.com>
2021-07-14 14:01:42 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import Todo from '../types/Todo';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = process.env.REACT_APP_BACKEND_SERVER_URL;
 
 export default {
   async add(title: string) {
-    return await axios.post<Todo>(baseUrl, {
+    return await axios.post<Todo>(baseUrl as string, {
       title,
       completed: false
     });
   },
   async fetch() {
-    return await axios.get<Todo[]>(baseUrl);
+    return await axios.get<Todo[]>(baseUrl as string);
   },
   async delete(todo: Todo) {
-    await axios.delete(todo.id.toString());
+    await axios.delete(`${baseUrl}/${todo.id}`);
   },
   async update(todo: Todo) {
-    return await axios.patch<Todo>(todo.id.toString(), todo);
+    return await axios.patch<Todo>(`${baseUrl}/${todo.id}`, todo);
   }
 };
